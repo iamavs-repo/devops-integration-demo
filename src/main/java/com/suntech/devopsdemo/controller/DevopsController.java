2fa378110a64a86257b4ef5eaa4f24d9a23c05d6
iamavs-repo <ashutoshvikram5897@gmail.com>
2023-06-30 12:34:14 +0530

    first commit

---
+++ src/main/java/com/suntech/devopsdemo/controller/DevopsController.java
@@ -0,0 +1,15 @@
+package com.suntech.devopsdemo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DevopsController {
+	
+	@GetMapping("/checkIt")
+	public String checking()
+	{
+		return "Devops Integration Project Is Working Fine";
+	}
+
+}
