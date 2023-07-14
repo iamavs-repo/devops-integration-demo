6834253ead17fc2f161e62bcb119bb7fe7660b37
iamavs-repo <133089277+iamavs-repo@users.noreply.github.com>
2023-07-14 12:00:21 +0530

    Update DevopsController.java

--- src/main/java/com/suntech/devopsdemo/controller/DevopsController.java
+++ src/main/java/com/suntech/devopsdemo/controller/DevopsController.java
@@ -2,6 +2,8 @@ package com.suntech.devopsdemo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 
 @RestController
 @RequestMapping("/service2")
